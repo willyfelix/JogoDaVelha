dc06ea5915cb4658660c401ae2a51b516e9d2dc9
willyfelix <williane.0730@gmail.com>
2022-01-19 19:25:03 -0300

    2 subIda

--- SRI-WillianeFelixDaCruzGoncalves.js
+++ SRI-WillianeFelixDaCruzGoncalves.js
@@ -11,61 +11,189 @@
  *
  */
 
-
-// este exemplo requer a instalação da biblioteca prompt-sync
-// comando para instalação: npm install prompt-sync
-// mais detalhes em https://www.npmjs.com/package/prompt-sync
-
-
 const prompt = require('prompt-sync')();
 
-// para adicionar mais opções ao menu altere esta função
+function main() {
+	let opcao = -1;
+
+	do {
+		imprimirMenu()
+		opcao = Number(prompt('Digite uma opção: '))
+
+		switch (opcao) {
+			case 1:
+				jogar()
+				break
+			case 2:
+				sobre()
+				break
+		}
+	} while (opcao != 3);
+}
+
 function imprimirMenu() {
-	console.log("===== JOGO DA VELHA =====")
+	console.log("\n=============== JOGO DA VELHA ====================\n");
 	console.log('Opções:')
 	console.log('(1) Jogar')
 	console.log('(2) Sobre')
 	console.log('(3) Sair')
 }
 
-const jogador1 = "X";
-const jogador2 = "O";
-var jogadordaVez = jogador1;
-var fimdejogo = false;
-
-function jogar() {
-
-   
-}
-function verificaSeVenceu() {
-
-    for(i = 0; i; );
-
-}
-
 function sobre() {
+	console.log("\n")
 	console.log("Instituto Federal de Educacao, Ciencia e Tecnologia - IFPE")
 	console.log("Cursos: Informatica para Internet | Sistemas para Internet")
- 	console.log("Componentes Curriculares: Logica de Programacao e Estrutura de Dados | Introducao a Programacao Imperativa")
- 	console.log("Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br")	
-    console.log("Aluna: Williane Felix - wfcg@discente.ifpe.edu.br")	
+	console.log("Componentes Curriculares: Logica de Programacao e Estrutura de Dados | Introducao a Programacao Imperativa")
+	console.log("Professor: Allan Lima - allan.lima@igarassu.ifpe.edu.br")
+	console.log("Aluna: Williane Felix - wfcg@discente.ifpe.edu.br")
+	console.log("\n")
 }
- 
-var opcao = -1
 
-do { 
+function jogar() {
+	let jogador, maquina, minhaVez = true;
+	const tabuleiro = [
+		["", "", ""],
+		["", "", ""],
+		["", "", ""],
+	];
 
-	imprimirMenu()
+	console.log("\nEscolha com qual peça deseja jogar:");
+	console.log("1) X");
+	console.log("2) O");
+	let opcao = Number(prompt('Digite uma opção: '));
+	console.log("\n");
 
-	opcao = Number(prompt('Digite uma opção: '))
-
-    switch (opcao) {
+	switch (opcao) {
 		case 1:
-			jogar()
-			break
+			jogador = "X";
+			maquina = "O";
+			break;
 		case 2:
-			sobre()
-			break
+			jogador = "O";
+			maquina = "X";
+			break;
+		default:
+			console.log("Opção inválida")
+			return;
 	}
 
-} while (opcao != 3)
+
+	let fimdejogo = false;
+	do {
+		imprimirTabuleiro(tabuleiro);
+
+		if (minhaVez == true) {
+			turnoJogador(jogador, tabuleiro);
+			minhaVez = false;
+		} else {
+			turnoMaquina(maquina, tabuleiro);
+			minhaVez = true;
+		}
+
+		fimdejogo = verificaSeVenceu(tabuleiro)
+	} while (fimdejogo == false);
+
+	if (minhaVez == false) {
+		console.log("Parabéns você venceu! Jogo finalizado!\n\n")
+	} else {
+		console.log("Não foi dessa vez, a máquina venceu! Jogo finalizado!\n\n")
+	}
+
+}
+
+function imprimirTabuleiro(tabuleiro) {
+	console.log("\n");
+	for (let i = 0; i < tabuleiro.length; i++) {
+		console.log(tabuleiro[i][0], "|", tabuleiro[i][1], "|", tabuleiro[i][2]);
+		console.log("________")
+	}
+	console.log("\n");
+}
+
+function turnoJogador(jogador, tabuleiro) {
+	console.log("\nÉ a sua vez!\n");
+
+	let linha, coluna;
+	let jogadaEValida = false;
+	while (jogadaEValida == false) {
+		linha = Number(prompt('Escolha uma linha (começando por zero): '));
+		coluna = Number(prompt('Escolha uma coluna (começando por zero): '));
+		jogadaEValida = verificarSeJogadaEValida(tabuleiro, linha, coluna);
+		if (jogadaEValida == false) {
+			console.log("Jogada inválida!\n")
+		}
+	}
+
+	tabuleiro[linha][coluna] = jogador;
+}
+
+function turnoMaquina(maquina, tabuleiro) {
+	console.log("\nÉ a vez do computador!\n");
+
+	let linha, coluna;
+	let jogadaEValida = false;
+	while (jogadaEValida == false) {
+		linha = parseInt(Math.random() * 3);
+		coluna = parseInt(Math.random() * 3);
+		jogadaEValida = verificarSeJogadaEValida(tabuleiro, linha, coluna);
+	}
+
+	tabuleiro[linha][coluna] = maquina;
+}
+
+function verificarSeJogadaEValida(tabuleiro, linha, coluna) {
+	if (linha < 0 || linha > 2) {
+		return false;
+	}
+
+	if (coluna < 0 || coluna > 2) {
+		return false;
+	}
+
+	const jaEstaPreenchido = tabuleiro[linha][coluna] != "";
+	if (jaEstaPreenchido == true) {
+		return false;
+	}
+}
+
+function verificaSeVenceu(tabuleiro) {
+	let resultado = false;
+
+	// verifica se alguma linha foi vencedora
+	for (let i = 0; i < tabuleiro.length; i++) {
+		if (tabuleiro[i][0] != "" && tabuleiro[i][1] != "" && tabuleiro[i][2] != "") {
+			if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
+				resultado = true;
+				break;
+			}
+		}
+	}
+
+	// verifica se alguma coluna foi vencedora
+	for (let i = 0; i < tabuleiro.length; i++) {
+		if (tabuleiro[0][i] != "" && tabuleiro[1][i] != "" && tabuleiro[2][i] != "") {
+			if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) {
+				resultado = true;
+				break;
+			}
+		}
+	}
+
+	// verifica se a diagonal principal foi vencedora
+	if (tabuleiro[0][0] != "" && tabuleiro[1][1] != "" && tabuleiro[2][2] != "") {
+		if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
+			resultado = true;
+		}
+	}
+
+	// verifica se a diagonal secundaria foi vencedora
+	if (tabuleiro[0][2] != "" && tabuleiro[1][1] != "" && tabuleiro[2][0] != "") {
+		if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
+			resultado = true;
+		}
+	}
+
+	return resultado;
+}
+
+main();
